3f17daf07d6ccbaac064a306d81bd41ea8ca25b2
Junior Fontenele <github@juniorfontenele.com.br>
2025-07-27 09:24:47 -0300

    fix: correct formatting and update documentation URL in Qualitor API credentials

--- credentials/QualitorApi.credentials.ts
+++ credentials/QualitorApi.credentials.ts
@@ -1,44 +1,44 @@
-import { ICredentialType, INodeProperties } from "n8n-workflow";
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
 
-export class QualitorApi implements ICredentialType { 
-  name = 'qualitorApi';
-  displayName = 'Qualitor API';
-  documentationUrl = '';
+export class QualitorApi implements ICredentialType {
+	name = 'qualitorApi';
+	displayName = 'Qualitor API';
+	documentationUrl = 'https://www.qualitor.com.br/docs/8.10.08.20170713/integracao/index.html';
 
-  properties: INodeProperties[] = [
-    {
-      displayName: 'Qualitor Base URL',
-      name: 'baseUrl',
-      type: 'string',
-      default: '',
-      placeholder: 'https://qualitor.example.com/qualitor',
-    },
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Qualitor Base URL',
+			name: 'baseUrl',
+			type: 'string',
+			default: '',
+			placeholder: 'https://qualitor.example.com/qualitor',
+		},
 
-    {
-      displayName: 'Company ID',
-      name: 'companyId',
-      type: 'number',
-      default: 1,
-      description: 'The ID of the company to which the user belongs',
-    },
+		{
+			displayName: 'Company ID',
+			name: 'companyId',
+			type: 'number',
+			default: 1,
+			description: 'The ID of the company to which the user belongs',
+		},
 
-    {
-      displayName: 'Username',
-      name: 'username',
-      type: 'string',
-      default: '',
-      description: 'The username for authentication',
-    },
+		{
+			displayName: 'Username',
+			name: 'username',
+			type: 'string',
+			default: '',
+			description: 'The username for authentication',
+		},
 
-    {
-      displayName: 'Password',
-      name: 'password',
-      type: 'string',
-      typeOptions: {
-        password: true,
-      },
-      default: '',
-      description: 'The password for authentication',
-    },
-  ];
-};
+		{
+			displayName: 'Password',
+			name: 'password',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+			description: 'The password for authentication',
+		},
+	];
+}
